6be33c5d2187dc25be9f1a4011431d188b46bd9b
donairos <61938923+donairos@users.noreply.github.com>
2020-04-09 18:48:36 +0800

    Add files via upload

--- index.php
+++ index.php
@@ -13,6 +13,10 @@
     <link href="https://fonts.googleapis.com/css?family=Montserrat:300&display=swap" rel="stylesheet">
     <script src="https://unpkg.com/typewriter-effect/dist/core.js"></script>
     <script src="https://kit.fontawesome.com/198be9e77a.js" crossorigin="anonymous"></script>
+    <script src="https://unpkg.com/ionicons@5.0.0/dist/ionicons.js"></script>
+    <link href="aos-master/dist/aos.css" rel="stylesheet">
+    <script src="aos-master/dist/aos.js"></script>
+     <link rel="stylesheet" href="animate.css/animate.min.css">
 </head>
 <style>
 
@@ -98,23 +102,6 @@
       text-decoration: none;
     }
 
-    .d_mbutton a {
-      text-align: center;
-     -webkit-animation: glow 1s ease-in-out infinite alternate;
-     -moz-animation: glow 1s ease-in-out infinite alternate;
-      animation: glow 1s ease-in-out infinite alternate;
-    }
-
-    @-webkit-keyframes glow {
-     from {
-      text-shadow: 0 0 10px #fff, 0 0 20px #fff, 0 0 30px #e60073, 0 0 40px #e60073, 0 0 50px #e60073, 0 0 60px #e60073, 0 0 70px #e60073;
-    }
-  
-     to {
-    text-shadow: 0 0 20px #fff, 0 0 30px #ff4da6, 0 0 40px #ff4da6, 0 0 50px #ff4da6, 0 0 60px #ff4da6, 0 0 70px #ff4da6, 0 0 80px #ff4da6;
-      }
-    }
-
     .p-3 img{
       width: 420px;
       max-width: 100%;
@@ -141,6 +128,43 @@
       cursor: pointer;
       text-shadow: 0 0 20px white;
     }
+
+    @media (max-width: 576px) {
+      .navbar{
+        background-color: white;
+      }
+
+      .navbar-nav .nav-item .nav-link{
+      color: black;
+      font-family: 'Montserrat', sans-serif;
+      letter-spacing: 1.5px;
+      }
+
+      .navbar-nav .nav-item .nav-link:hover{
+      border-bottom: none;
+      font-weight: bold;
+      color: indigo;
+      opacity: 1;
+      }
+
+      .navbar .navbar-brand{
+      margin-left: 11%;
+      }
+
+
+    }
+
+    .page-footer{
+      background-color: #e6e6e6;
+    }
+
+    ion-icon{
+      color: red;
+    }
+
+   
+
+    
 
    	
 
@@ -193,22 +217,133 @@
   </div>
   <br>
   <br>
-  <hr>
-
-  <div class="d-flex flex-wrap">
-    <div class="p-4"><a href="https://www.github.com/donairos"> <i class="fab fa-github"> </i> /donairos </a> </div>
-    <div class="p-4"><a href="https://www.facebook.com/yashieey"><i class="fab fa-facebook"></i> /yashieey </a> </div>
-    <div class="p-4"><a href="https://www.facebook.com/yashieey"><i class="fab fa-twitter"></i> /d_onairos </a></div>
-    <div class="p-4"><a href="https://www.facebook.com/yashieey"><i class="fab fa-instagram"></i> /d.onairos </a></div>
-
-  </div>
-
 	</div>
-
-
-
+  <br><br><br><br><br><br>
+  
 
 </body>
+<script>
+  AOS.init();
+</script>
+
+
+<!-- Footer -->
+<footer class="page-footer font-small unique-color-dark">
+
+  <div style="background-color: white;">
+    <div class="container">
+
+      <!-- Grid row-->
+      <div class="row py-4 d-flex align-items-center animated shake delay-2s">
+
+        <!-- Grid column -->
+        <div class="col-md-6 col-lg-5 text-center text-md-left mb-4 mb-md-0">
+          <h6 class="mb-0">Get connected with me on other social platforms!</h6>
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-6 col-lg-7 text-center text-md-right">
+
+          <!-- Github-->
+          <a class="github-ic">
+            <a style="text-decoration: none; color: black;" href="https://www.github.com/donairos" target="_blank"><i class="fab fa-github white-text mr-4"> </i></a>
+          </a>
+          <!-- Facebook -->
+          <a class="fb-ic">
+            <a style="text-decoration: none; color: black;" href="https://www.facebook.com/yashieey" target="_blank"><i class="fab fa-facebook-f white-text mr-4"> </i></a>
+          </a>
+          <!-- Twitter -->
+          <a class="tw-ic">
+            <i class="fab fa-twitter white-text mr-4"> </i>
+          </a>
+          <!--Instagram-->
+          <a class="ins-ic">
+            <i class="fab fa-instagram white-text"> </i>
+          </a>
+
+        </div>
+        <!-- Grid column -->
+
+      </div>
+      <!-- Grid row-->
+
+    </div>
+  </div>
+
+  <!-- Footer Links -->
+  <div class="container text-center text-md-left mt-5">
+
+    <!-- AOS Fade up-->
+    <div data-aos="fade-up">
+
+    <!-- Grid row -->
+    <div class="row mt-3">
+
+      <!-- Grid column -->
+      <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+
+        <!-- Content -->
+        <h6 class="text-uppercase font-weight-bold">I'm Den</h6>
+        <hr class="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+        <p>Hi there, welcome aboard feel free to know me more with my portfolio. </p>
+
+      </div>
+      <!-- Grid column -->
+
+      <!-- Grid column -->
+      <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+
+        <!-- Links -->
+        <h6 class="text-uppercase font-weight-bold">Useful links</h6>
+        <hr class="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+        <p>
+          <a style="color: #03358c;" href="#!">About</a>
+        </p>
+        <p>
+          <a style="color: #03358c;" href="#!">Projects</a>
+        </p>
+        <p>
+          <a style="color: #03358c;" href="#!">Contact</a>
+        </p>
+
+      </div>
+      <!-- Grid column -->
+
+      <!-- Grid column -->
+      <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+
+        <!-- Links -->
+        <h6 class="text-uppercase font-weight-bold">Connect with me</h6>
+        <hr class="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+
+        <p>
+          <i class="fas fa-envelope mr-3"></i> kinz.onairos@gmail.com</p>
+        <p>
+          <i class="fas fa-phone mr-3"></i> + 63 908 166 8520</p>
+
+      </div>
+      <!-- Grid column -->
+
+    </div>
+    <!-- Grid row -->
+
+   </div>
+    <!-- AOS FADE -->
+
+  </div>
+  <!-- Footer Links -->
+
+  <!-- Copyright -->
+  <div class="footer-copyright text-center py-3">
+    Made with <ion-icon name="heart"></ion-icon> by <a href="https://www.facebook.com/yashieey"> Den Soriano </a>
+  </div>
+  <!-- Copyright -->
+
+</footer>
+<!-- Footer -->
+
+
 <script>
   var app = document.getElementById('app');
 
