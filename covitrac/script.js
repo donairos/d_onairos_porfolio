43466df99ce235f60e58e55ffce10f8549534a52
donairos <61938923+donairos@users.noreply.github.com>
2020-06-01 15:08:43 +0800

    Add files via upload

--- covitrac/script.js
+++ covitrac/script.js
@@ -5,19 +5,21 @@ async function getConfirmed() {
   const data = await response.json();
   const { infected, recovered, deceased, country } = data;
 
+  console.log(data);
+
   document.getElementById("country").textContent = country;
 
-  const countUp = new CountUp("infected", 0, infected, 0, 1.8);
+  const countUp = new CountUp("infected", 0, infected, 0, 2.0);
   countUp.start();
+  console.log("PH Infected " + infected);
 
-  const countUp2 = new CountUp("recovered", 0, recovered, 0, 1.8);
+  const countUp2 = new CountUp("recovered", 0, recovered, 0, 2.0);
   countUp2.start();
+  console.log("PH Recovered " + recovered);
 
-  const countUp3 = new CountUp("death", 0, deceased, 0, 1.8);
+  const countUp3 = new CountUp("death", 0, deceased, 0, 2.0);
   countUp3.start();
-
-  document.getElementById("recovered").textContent = recovered;
-  console.log(data);
+  console.log("PH Deaths " + deceased);
 }
 
 const global_url = "https://api.covid19api.com/summary";
@@ -28,17 +30,42 @@ async function getSummary() {
     Global: { TotalConfirmed, TotalRecovered, TotalDeaths },
   } = data;
 
-  const countUp4 = new CountUp("infected-global", 0, TotalConfirmed, 0, 1.8);
+  console.log(data);
+
+  const countUp4 = new CountUp("infected-global", 0, TotalConfirmed, 0, 2.0);
   countUp4.start();
+  console.log("Total Infected " + TotalConfirmed);
 
-  const countUp5 = new CountUp("recovered-global", 0, TotalRecovered, 0, 1.8);
+  const countUp5 = new CountUp("recovered-global", 0, TotalRecovered, 0, 2.0);
   countUp5.start();
+  console.log("Total Recovered " + TotalRecovered);
 
-  const countUp6 = new CountUp("death-global", 0, TotalDeaths, 0, 1.8);
+  const countUp6 = new CountUp("death-global", 0, TotalDeaths, 0, 2.0);
   countUp6.start();
-
-  console.log(TotalConfirmed);
+  console.log("Total Deaths " + TotalDeaths);
 }
+
+async function getTableData() {
+  const response = await fetch(global_url);
+  const data = await response.json();
+  const { Countries } = data;
+  let output = "";
+  Countries.forEach((c) => {
+    output += "<tr>";
+    output += "<td>" + c.Country + "</td>";
+    output += "<td>" + c.TotalConfirmed + "</td>";
+    output += "<td>" + c.NewConfirmed + "</td>";
+    output += "<td>" + c.TotalRecovered + "</td>";
+    output += "<td>" + c.NewRecovered + "</td>";
+    output += "<td>" + c.TotalDeaths + "</td>";
+    output += "</td>";
+  });
+  document.getElementById("globaldata").innerHTML = output;
+}
+
+
 
 getConfirmed();
 getSummary();
+getTableData();
+
